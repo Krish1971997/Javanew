64a444fddabeb81bec050000d14b512b3ed2d2c2
Krish1971997 <krishnakumar1971997@gmail.com>
2025-07-03 23:11:03 +0530

    Commit on 03/07/2025 23:11:02.78

--- src/zProgrammingRound/StringManipulation.java
+++ src/zProgrammingRound/StringManipulation.java
@@ -3,8 +3,8 @@ package zProgrammingRound;
 public class StringManipulation {
 	public static void main(String[] args) {
 		// case 1
-		// String input="aaaaaaa";
-		// String pattern="aa"; //Output: Xa
+//		 String input="aaaaaaa";
+//		 String pattern="aa"; //Output: Xa
 		// case 2
 		String input = "abdekwabdkjerwkbabdkl";
 		String pattern = "abd"; // Output XekwXkjerwkbXkl
@@ -16,18 +16,20 @@ public class StringManipulation {
 		StringBuilder output = new StringBuilder();
 		int previousIndex = -1;
 		int patternLength = pattern.length();
-		for (int i = 0; i < input.length(); i++) {
+		int i = 0;
+		while (i < input.length()) {
 			if (input.length() >= i + patternLength && input.substring(i, i + patternLength).equals(pattern)) {
-				if (i == previousIndex) {
-					previousIndex = patternLength + i + 1;
-					i += patternLength;
+
+				previousIndex = patternLength + i;
+				i = i + patternLength;
+
+				if (i == previousIndex)
 					continue;
-				}
-				previousIndex = i + patternLength + 1;
-				i = i + patternLength - 1;
+
 				output.append("X");
+
 			} else {
-				output.append(input.charAt(i));
+				output.append(input.charAt(i++));
 			}
 		}
 
