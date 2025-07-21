42e737dc30fa86337aa68930c88fe33fa8f6f4af
Krish1971997 <118267997+Krish1971997@users.noreply.github.com>
2025-07-21 20:28:35 +0530

    Create FindTheHighestAltitude.java

---
+++ src/leetcodeZoho6/FindTheHighestAltitude.java
@@ -0,0 +1,21 @@
+package test4;
+
+public class FindTheHighestAltitude {
+	public static void main(String[] args) {
+		int[] arr= {-4,-3,-2,-1,4,3,2};
+		System.out.println(findnumber(arr));
+	}
+
+	private static int findnumber(int[] arr) {
+		
+		int num=0;
+		int max=0;
+		for (int i = 0; i < arr.length; i++) {
+			num+=arr[i];
+			max=Math.max(max, num);
+		}
+		
+		return max;
+	}
+
+}
